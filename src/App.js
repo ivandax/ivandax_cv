b45e53bf7628e549253a8edfdfac294796e7d5d6
ivandax <ivan.dario@nexiona.com>
2021-11-20 19:12:48 +0100

    Add firebase

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ import { IoMdBriefcase, IoMdBusiness } from "react-icons/io";
 import { AiOutlineMail, AiFillPhone, AiOutlineCalendar } from "react-icons/ai";
 import { BsGlobe } from "react-icons/bs";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBH6Eixd4YI9Pd-KJHczujVqSp1DFM2i3o",
+  authDomain: "ivandax-cv.firebaseapp.com",
+  projectId: "ivandax-cv",
+  storageBucket: "ivandax-cv.appspot.com",
+  messagingSenderId: "349019642640",
+  appId: "1:349019642640:web:07da3c46304b302038a395",
+  measurementId: "G-X6NGRCE119"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 function App() {
   return (
     <body style={{ paddingTop: "12px" }}>
