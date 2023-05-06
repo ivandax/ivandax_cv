d24eb2f0b1ebc3ac2009cf9d142d795dcd3a54af
ivandax <ivandariogye@gmail.com>
2023-05-06 20:54:37 +0200

    Add basic content for promises

--- src/views/Content.js
+++ src/views/Content.js
@@ -21,12 +21,14 @@ function Content() {
               padding: "10px",
               justifyContent: "flex-start",
               alignItems: "flex-start",
+              flexDirection: "column",
             }}
           >
             <Tabs
               activeSection={activeSection}
               setActiveSection={setActiveSection}
             />
+            <SectionContent activeSection={activeSection} />
           </div>
         </div>
       </div>
@@ -50,92 +52,120 @@ function Tabs({ activeSection, setActiveSection }) {
         justifyContent: "space-evenly",
       }}
     >
-      <button
-        onClick={() => setActiveSection("event-loop")}
-        style={{
-          margin: 0,
-          padding: "10px",
-          border: "none",
-          backgroundColor:
-            activeSection === "event-loop" ? "#2952a3" : "#849ac5",
-          color: activeSection === "event-loop" ? "white" : "black",
-          flexGrow: 1,
-          cursor: "pointer",
-        }}
-      >
-        Event Loop
-      </button>
-      <button
-        onClick={() => setActiveSection("promises")}
-        style={{
-          margin: 0,
-          padding: "10px",
-          backgroundColor: activeSection === "promises" ? "#2952a3" : "#849ac5",
-          color: activeSection === "promises" ? "white" : "black",
-          border: "none",
-          flexGrow: 1,
-          cursor: "pointer",
-        }}
-      >
-        Promises
-      </button>
-      <button
-        onClick={() => setActiveSection("closures")}
-        style={{
-          margin: 0,
-          padding: "10px",
-          backgroundColor: activeSection === "closures" ? "#2952a3" : "#849ac5",
-          color: activeSection === "closures" ? "white" : "black",
-          border: "none",
-          flexGrow: 1,
-          cursor: "pointer",
-        }}
-      >
-        Closures
-      </button>
-      <button
-        onClick={() => setActiveSection("inheritance")}
-        style={{
-          margin: 0,
-          padding: "10px",
-          backgroundColor:
-            activeSection === "inheritance" ? "#2952a3" : "#849ac5",
-          color: activeSection === "inheritance" ? "white" : "black",
-          border: "none",
-          flexGrow: 1,
-          cursor: "pointer",
-        }}
-      >
-        Inheritance
-      </button>
-      <button
-        onClick={() => setActiveSection("fetch")}
-        style={{
-          margin: 0,
-          padding: "10px",
-          backgroundColor: activeSection === "fetch" ? "#2952a3" : "#849ac5",
-          color: activeSection === "fetch" ? "white" : "black",
-          border: "none",
-          flexGrow: 1,
-          cursor: "pointer",
-        }}
-      >
-        fetch
-      </button>
-      <button
-        onClick={() => setActiveSection("this")}
-        style={{
-          margin: 0,
-          padding: "10px",
-          backgroundColor: activeSection === "this" ? "#2952a3" : "#849ac5",
-          color: activeSection === "this" ? "white" : "black",
-          border: "none",
-          flexGrow: 1,
-          cursor: "pointer",
-        }}
-      >
-        this
-      </button>
+      <TabButton
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+        tab="event-loop"
+        label="Event Loop"
+      />
+      <TabButton
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+        tab="promises"
+        label="Promises"
+      />
+      <TabButton
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+        tab="closures"
+        label="Closures"
+      />
+      <TabButton
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+        tab="inheritance"
+        label="Inheritance"
+      />
+      <TabButton
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+        tab="fetch"
+        label="Fetch"
+      />
+      <TabButton
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+        tab="this"
+        label="this"
+      />
     </div>
   );
+}
+
+function TabButton({ label, tab, setActiveSection, activeSection }) {
+  return (
+    <button
+      onClick={() => setActiveSection(tab)}
+      style={{
+        margin: 0,
+        padding: "10px",
+        backgroundColor: activeSection === tab ? "#2952a3" : "#849ac5",
+        color: activeSection === tab ? "white" : "black",
+        border: "none",
+        flexGrow: 1,
+        cursor: "pointer",
+      }}
+    >
+      {label}
+    </button>
+  );
+}
+
+function SectionContent({ activeSection }) {
+  switch (activeSection) {
+    case "event-loop": {
+      return (
+        <div style={{ padding: "10px" }}>
+          <h3 style={{ margin: 0 }}>Event Loop</h3>
+          <p>Hello world</p>
+        </div>
+      );
+    }
+    case "promises": {
+      return (
+        <div style={{ padding: "10px" }}>
+          <h3 style={{ margin: 0 }}>Promises</h3>
+          <p>
+            A promise in Javascript allows us to handle asynchronous operations.
+            A typical case where we use promises is when calling a REST API, the
+            response might take some time. The promise is like a container for
+            the resulting value of that call.
+          </p>
+          <p>
+            The promise might resolve successfully or it might fail, so it has
+            three distinct states: "Pending", "Fullfilled" and "Rejected"
+          </p>
+          <pre>
+            <code>
+              {`
+              const myPromise = new Promise((resolve, reject) => {
+                // Do some asynchronous operation here
+                if (/* operation succeeds */) {
+                  resolve(result);
+                } else {
+                  reject(error);
+                }
+              });
+              `}
+            </code>
+          </pre>
+        </div>
+      );
+    }
+    case "closures": {
+      return <>Closures</>;
+    }
+    case "inheritance": {
+      return <>Inheritance</>;
+    }
+    case "fetch": {
+      return <>fetch</>;
+    }
+    case "this": {
+      return <>this</>;
+    }
+    default: {
+      return <>Not Found</>;
+    }
+  }
 }
